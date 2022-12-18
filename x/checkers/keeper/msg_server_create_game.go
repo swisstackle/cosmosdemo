4f2ea2b9c4fcbf1022b3d7a699902f3c2fa6fa5b
swisstackle <alainsch@buffalo.edu>
2022-12-17 20:09:19 -0500

    more tests

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+
+
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,9 +1,14 @@
 package keeper_test
 
 import (
-	"github.com/stretchr/testify/require"
-	"github.com/swisstackle/checkers/x/checkers/types"
+	"context"
 	"testing"
+	keepertest "github.com/swisstackle/checkers/testutil/keeper"
+	"github.com/swisstackle/checkers/x/checkers"
+	"github.com/swisstackle/checkers/x/checkers/keeper"
+	"github.com/swisstackle/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -12,16 +17,88 @@ const (
 	carol = "cosmos1426zv69mav7glynccqwfhlm5we73l2njud2ljn"
 )
 
-func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
-	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: alice,
-		Black:   bob,
-		Red:     carol,
-	})
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+    k, ctx := keepertest.CheckersKeeper(t)
+    checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+    return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
 
+func TestCreate1GameHasSaved(t *testing.T) {
+    msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+    createResponse, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "",
+		GameIndex: "1",
 	}, *createResponse)
+
+    systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+    require.True(t, found)
+    require.EqualValues(t, types.SystemInfo{
+        NextId: 2,
+    }, systemInfo)
+    game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+    require.True(t, found1)
+    require.EqualValues(t, types.StoredGame{
+        Index: "1",
+        Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+        Turn:  "b",
+        Black: bob,
+        Red:   carol,
+    }, game1)
+}
+
+func TestCreate3GameHasSaved(t *testing.T) {
+    msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	panic(ctx)
+    createResponse, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "1",
+	}, *createResponse)
+
+	createResponse2, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "2",
+	}, *createResponse2)
+
+	createResponse3, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "3",
+	}, *createResponse3)
+
+    systemInfo, found := keeper.GetSystemInfo(ctx)
+    require.True(t, found)
+    require.EqualValues(t, types.SystemInfo{
+        NextId: 4,
+    }, systemInfo)
+    game1, found1 := keeper.GetStoredGame(ctx, "1")
+    require.True(t, found1)
+    require.EqualValues(t, types.StoredGame{
+        Index: "1",
+        Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+        Turn:  "b",
+        Black: bob,
+        Red:   carol,
+    }, game1)
+
+	//game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
 }
